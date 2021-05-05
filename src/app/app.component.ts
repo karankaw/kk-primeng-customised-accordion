29ca373e6701f884ca1825043abd24384dc7996d
karankaw <karankaw@hotmail.com>
2021-05-05 22:57:56 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,35 @@
-import { Component } from '@angular/core';
-import {MessageService} from 'primeng/api';
+import { Component } from "@angular/core";
+import { MessageService } from "primeng/api";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
   providers: [MessageService]
 })
-export class AppComponent { 
-    activeState: boolean[] = [true, false, false];
+export class AppComponent {
+  activeState: boolean[] = [true, false, false];
 
-    constructor(private messageService: MessageService) {}
+  isEnabled : boolean = true;
 
-    onTabClose(event) {
-        this.messageService.add({severity:'info', summary:'Tab Closed', detail: 'Index: ' + event.index})
-    }
-    
-    onTabOpen(event) {
-        this.messageService.add({severity:'info', summary:'Tab Expanded', detail: 'Index: ' + event.index});
-    }
+  constructor(private messageService: MessageService) {}
 
-    toggle(index: number) {
-        this.activeState[index] = !this.activeState[index];
-    }
+  onTabClose(event) {
+    this.messageService.add({
+      severity: "info",
+      summary: "Tab Closed",
+      detail: "Index: " + event.index
+    });
+  }
+
+  onTabOpen(event) {
+    this.messageService.add({
+      severity: "info",
+      summary: "Tab Expanded",
+      detail: "Index: " + event.index
+    });
+  }
+
+  toggle(index: number) {
+    this.activeState[index] = !this.activeState[index];
+  }
 }
